8d02fb9306c48cd3a480a90af587b85b75c74a8c
Borkata <borispenev89@gmail.com>
2016-09-20 18:42:37 +0300

    adding a sample c# project

---
+++ Tech Module/Programming Fundamentals/02.Github-Exercises/01.Intro-Programming/WhichIsGreater.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Intro_Programming
+{
+    class WhichIsGreater
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine("X: {0} is greater", x);
+            }
+            else
+            {
+                Console.WriteLine("Y: {0} is greater", y);
+            }
+
+        }
+    }
+}
